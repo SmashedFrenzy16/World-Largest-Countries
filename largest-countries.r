ae76e39641455813213febf4779467dd4ea6664a
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-02-12 21:14:28 +0000

    Add files via upload

---
+++ largest-countries.r
@@ -0,0 +1,4 @@
+library(plotrix)
+x <- c(11, 6.3, 6.1, 6.1, 5.6, 5.2, 2.0, 1.8, 1.8, 1.6)
+lbl <- c("Russia", "China", "Canada", "USA", "Brazil", "Australia", "India", "Argentina", "Kazakstan", "Algeria")
+pie3D(x, labels = lbl, explode=0.1, main = "3D Pie Chart Of The World's Largest Countries (% Landmass)")
